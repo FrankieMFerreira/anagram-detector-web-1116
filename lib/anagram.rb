f68dbeeff88de39cd4a2d32c63ae60fa3ff983c8
FrankieMFerreira <FrankieMFerreira@gmail.com>
2016-12-01 11:31:51 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
 # Your code goes here!
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(input)
+    match_test = @word.split("").sort
+
+    input.select do |word|
+    if word.split("").sort == match_test
+      word
+    end
+  end
+end
+
+end
